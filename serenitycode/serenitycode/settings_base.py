8e825f9dbab9815c69f108d09de994db8fd2ec0a
Jeffrey Whitehead <jeff@serenitycode.co.uk>
2014-12-14 10:46:13 +0000

    Remove Django comment filler from settings

--- serenitycode/serenitycode/settings_base.py
+++ serenitycode/serenitycode/settings_base.py
@@ -1,33 +1,19 @@
 """
 Django settings for serenitycode project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+Obviously settings are changed in production - just not storing those settings
+in this repo.
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'el6m)s$eyad+wxt%i=9on#hxpg3^vavjq1eamrgfww#(gm#%uh'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -52,10 +38,6 @@ ROOT_URLCONF = 'serenitycode.urls'
 
 WSGI_APPLICATION = 'serenitycode.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -66,8 +48,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -78,9 +58,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
