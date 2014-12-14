ca34a7d0f7a3e34b1fd1bc3b1cce17734308ef93
Jeffrey Whitehead <jeff@serenitycode.co.uk>
2014-12-14 11:51:43 +0000

    Updated urls file to include new apps

--- serenitycode/serenitycode/urls.py
+++ serenitycode/serenitycode/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'serenitycode.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('pages.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
