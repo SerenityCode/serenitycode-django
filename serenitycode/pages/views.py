527185ce4adab5fe0b65b053831d12a72ca0573c
Jeffrey Whitehead <jeff@serenitycode.co.uk>
2015-01-23 18:17:31 +0000

    order blog posts and projects

--- serenitycode/pages/views.py
+++ serenitycode/pages/views.py
@@ -4,12 +4,12 @@ from .models import Project
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-publish_date')
     context = {'posts': posts}
     return render(request, 'pages/index.html', context)
 
 
 def projects_view(request):
-    projects = Project.objects.all()
-    context = {'projects' : projects}
+    projects = Project.objects.all().order_by('-last_modified')
+    context = {'projects': projects}
     return render(request, 'pages/projects.html', context)
